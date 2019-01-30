e4eac2d6321a0651095ee6e37c39f59c76a14783
Max Silver <Maxbsilver@gmail.com>
2019-01-30 15:39:24 -0700

    Update submite guess and name

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 // GLOBAL VARIABLES
 
 var updateBtn = document.querySelector('.update-btn');
+var submitBtn = document.querySelector('.submit-guess-btn');
 var displayCurMin = document.querySelector('.cur-min');
 var displayCurMax = document.querySelector('.cur-max');
-var challengerOneName = document.querySelector('.chal-name-1');
-var challengerOneGuess = document.querySelector('.chal-guess-1');
-var challengerTwoName = document.querySelector('.chal-name-2');
-var challengerTwoGuess = document.querySelector('.chal-guess-2');
+var chalName1Input = document.querySelector('.chal-name-1');
+var chalName2Input = document.querySelector('.chal-name-2');
 
 // EVENT LISTENERS
 
 updateBtn.addEventListener('click', setRange);
+submitBtn.addEventListener('click', submitGuess)
+
+
+
+
 // FUNCTIONS
 
-// Grab minInput and maxInput and create a conditional range 
-// print both to respective html places
-// use the inputted values as conditionals for the guess
 
 function setRange(event) {
  
@@ -29,8 +30,36 @@ function setRange(event) {
 }
 
 function randomNumber(min, max) {
-// Create a random number
-// With parameters between the minInput and maxInput
+
   var rando = Math.floor(Math.random()*(max-min))+ min;
   console.log(rando);
+
 }
+
+
+
+
+
+
+function submitGuess(event) {
+    
+    event.preventDefault();
+   
+    var guessOne = parseInt(document.querySelector('.chal-guess-1').value);
+    var guessTwo = parseInt(document.querySelector('.chal-guess-2').value);
+    var guessOneDisplay = document.querySelector('.guess-display-1');
+    var guessTwoDisplay = document.querySelector('.guess-display-2');
+    var lsNameChange1 = document.querySelector('.updated-name-1');
+    var lsNameChange2 = document.querySelector('.updated-name-2');
+
+    guessOneDisplay.innerText = guessOne;
+    guessTwoDisplay.innerText = guessTwo;
+    lsNameChange1.innerText = chalName1Input.value;
+    lsNameChange2.innerText = chalName2Input.value;
+
+}
+
+// submit guesses
+// var for each 5 input 
+// change the latest score values for name and guess
+//names should be local and guesses should be global
